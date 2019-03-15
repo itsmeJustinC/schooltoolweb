6abab8005897bddc97116d2c6d89c6c1b33be86a
itsmeJustinC <36489899+itsmeJustinC@users.noreply.github.com>
2019-03-15 19:33:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 
 username = "Pb272098" #fill with user input
