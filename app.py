708d643870a1b93fc6892f91c8bedc0503c3f6b3
itsmeJustinC <36489899+itsmeJustinC@users.noreply.github.com>
2019-03-26 12:00:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+#######################################################################################
+# This program awaits a request from a website then given the proper credentials,
+# will get the grades from our school grading system and return them to the requesting
+# website so that it can be displayed nicely for the user
+#######################################################################################
+
+# Imports of various libraries that run the Flask server, webdriver, Timers and CORS
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from threading import Timer
@@ -6,7 +13,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
-
+# Translate the actual quarter to the value used on schooltool
 QUARTERS = {
   "1" : "155",
   "2" : "154",
@@ -14,10 +21,14 @@ QUARTERS = {
   "4" : "152"
 }
 
+# The URL that selenium get pointed to (The schooltool website)
 URL = "https://schooltool.pinebushschools.org/schooltoolweb/"
 
+# This dictionary is what holds the cached users info fo five minutes 
 content_dict = {}
 
+# This function deletes a users info from "content_dict" if it is present in the dictionary
+# Otherwise it prints "No cached data with that username"
 def delete_content(username):
   if (username in content_dict):
     del content_dict[username]
@@ -25,9 +36,18 @@ def delete_content(username):
   else:
     print("No cached data with that username")
 
+# This instantiates the flask app
 app = Flask(__name__)
+# This activates cross origin resource sharing for the flask app so that the
+# front end can make requests to the flask app
 CORS(app)
 
+# This sets the chrome options for the webdriver
+# It does a couple of things
+# 1)It puts the webdriver into headless mode to save a little time with not having to display
+# the webbrowser since it wouldn't been seen by the user anyway
+# 2)It tells the webdriver not to load images because that takes up a significant amount of time
+# when running the program (About 10 seconds which is alot!).
 def set_chrome_options():
   chrome_options = Options()
   chrome_options.add_argument("--headless")
@@ -35,6 +55,9 @@ def set_chrome_options():
   chrome_options.add_experimental_option("prefs", prefs)
   return chrome_options
 
+# This function takes in the username, password, and quarter as parameters and then uses
+# these to sign into the user schooltool and extract their averages in each of their
+# classes for the specified quarter in a dictionary
 def get_grades(url, username, password, quarter):
   driver = webdriver.Chrome(chrome_options=set_chrome_options())
   
@@ -67,6 +90,8 @@ def get_grades(url, username, password, quarter):
     
   return return_grades
 
+# Given a username and quarter as parameters, this function adds the user's info to the content dictionary
+# So that it could be retrieved very fast if the user requsted them again in the span of five minutes
 def add_to_content_dict(username, quarter):
   username_dict = username + quarter
   content_dict[username_dict] = return_grades
@@ -74,8 +99,15 @@ def add_to_content_dict(username, quarter):
   Timer(300, delete_content, [username_dict]).start()
   return jsonify(return_grades)
 
-@app.route('/', methods=["GET", "POST"])
+# This is a function decorator that specifies the route that the flask server is located at
+# it also specifies the methods in which it can be accessed. This route can only be accessed via a POST
+# request and couldn't be accessed typing its URL into google
+@app.route('/', methods=["POST"])
+# This function dictates what to do when a POST request is received
 def index():
+  # This bit gets the username, password, and quarter number of the user
+  # The if statement checks if the user has cached data that could be returned instead of running
+  # the get_grades function which takes a little bit of time
   json = request.get_json()
   username = json['username']
   passwd = json['password']
@@ -83,12 +115,15 @@ def index():
   if (username + quarter in content_dict):
     return content_dict[username + quarter]
   
+  # Return the grades of the user as a JSON object for the front end to use
   return jsonify(get_grades(URL, username, passwd, quarter))
 
+# This handles a 500 error which could occur if the username and password is incorrect
+# If this happens it instead returns a 401 error that is returned to the front end
 @app.errorhandler(500)
 def internal_server_error(error):
   return "401 error", 401
 
-
+# This just runs the program
 if __name__ == "__main__":
   app.run(debug=True, use_reloader=True)
