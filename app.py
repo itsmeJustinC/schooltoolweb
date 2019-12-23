7f7882b131e057f876e4c000e3afd47ee134dfa9
itsmeJustinC <36489899+itsmeJustinC@users.noreply.github.com>
2019-12-23 18:53:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -69,8 +69,7 @@ def get_grades(url, username, password, quarter):
   
   driver.find_element_by_xpath('//*[@title="View Student Record"]').click()
 
-
-  driver.find_element_by_id('Template1_Control0_TabHeader2_Menutabs1_MenuTabGrades').click()
+  driver.find_element_by_xpath('//*[@id="Template1_Control0_TabHeader2_Menutabs1_MenuTabGrades"]').click()
   select = Select(driver.find_element_by_name('Template1$Control0$StudentGradesView1$ViewDropDownList'))
   select.select_by_value('3')
 
